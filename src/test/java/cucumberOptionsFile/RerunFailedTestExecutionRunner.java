d8ff6e2ea5c4c6b0aa083feb5532ed692cec64d3
Rajagopal Arumugam <rajagopal.arumugam@genesys.com>
2022-06-15 14:17:14 +0530

    Integration of re-run failed test cases

---
+++ src/test/java/cucumberOptionsFile/RerunFailedTestExecutionRunner.java
@@ -0,0 +1,18 @@
+package cucumberOptionsFile;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = {"@target/failedScenarios.txt"}, glue = "steps", 
+monochrome=true, plugin= {"html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
+public class RerunFailedTestExecutionRunner extends AbstractTestNGCucumberTests{	
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios()
+	{
+		return super.scenarios();
+	}
+}
--- src/test/java/cucumberOptionsFile/TestExecutionRunner.java
+++ src/test/java/cucumberOptionsFile/TestExecutionRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/java/features"}, glue = "steps", tags="@Sanity or @Regression", 
-monochrome=true, plugin= {"html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+monochrome=true, plugin= {"html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failedScenarios.txt"	})
 
 public class TestExecutionRunner extends AbstractTestNGCucumberTests{	
 	@Override
